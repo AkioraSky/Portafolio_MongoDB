087b4e99f7ab272e28134fd75c5345cf8f25d2f1
kh2.0acru <kh2.0acru@gmail.com>
2023-11-28 01:18:30 -0500

    cuerpo4

--- front/src/components/main/CrearProyecto.jsx
+++ front/src/components/main/CrearProyecto.jsx
@@ -1,13 +1,111 @@
 import React from "react";
+import HelperForm from "../../helpers/HelperForm";
+import { Global } from "../../helpers/Global";
+import Swal2 from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+const MySwal = withReactContent(Swal2);
 
 const CrearProyecto = () => {
+  
+const inputdetalle = document.getElementById("detalle");
+const { form, cambiar } = HelperForm({});
+const token = localStorage.getItem("token");
+  const AgregarEstudio = async (e) => {
+      e.preventDefault();
+      let formulario = form;
+  
+      //guardar en la api
+  
+      const request = await fetch(Global.url + "estudios/crearEstudio", {
+        method: "POST",
+        body: JSON.stringify(formulario),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${token}`,
+        },
+      });
+      const data = await request.json();
+      if (data.id == 200) {
+        let titulo = data.Encabezado;
+        let mensaje = data.mensaje;
+        MySwal.fire({
+          title: <strong> {titulo}</strong>,
+          html: <i>{mensaje}</i>,
+          icon: "success",
+        });
+        inputdetalle.value = "";
+      } else {
+        let titulo = data.Encabezado;
+        let mensaje = data.mensaje;
+        MySwal.fire({
+          title: <strong> {titulo}</strong>,
+          html: <i>{mensaje}</i>,
+          icon: "error",
+        });
+      }
+    };
   return (
     <>
-      <main id="main">
-        <section class="inner-page">
-          <div class="container">
-            <p>Crear Proyecto</p>
-          </div>
+       <main id="main">
+        <section className="inner-page">
+        <div className="container">
+            <div className="text-center mb-3" style={{color:"white"}}>
+<h1>Agregar proyecto</h1>
+            </div>
+            <div className="row justify-content-center">
+            <div className="col">
+    
+    <form action="forms/contact.php" method="post" role="form" className="php-email-form"  onSubmit={AgregarEstudio}>
+      <div className="row" style={{color:"white"}} >
+        <label htmlFor=""> Estudio</label>
+      <select
+                name="tipo"
+                className="form-control form-control-"
+                id="tipo"
+                onChange={cambiar}
+                style={{ borderRadius: "50px", fontSize: "100%" }}
+              >
+                <option value="TECNICO">Tecnico</option>
+                <option value="TECNOLOGO">Tecnologo</option>
+                <option value="PROFESIONAL">Profesional</option>
+                <option value="ESPECIALIZACION">Especializacion</option>
+                <option value="MAESTRIA">Maestria</option>
+                <option value="DOCTORADO">Doctorado</option>
+              </select>
+        <div className="form-group">
+          <label for="name">Detalle</label>
+          <input type="text" className="form-control" name="detalle" id="detalle" required onChange={cambiar}/>
+        </div>
+        <div className="form-group">
+        <label for="name">Fecha</label>
+        <input type="date" className="form-control" name="fecha" id="fecha" required onChange={cambiar}/>
+      </div>
+      <div className="form-group">
+      <label for="name">Estado</label>
+      <select
+                name="notas"
+                className="form-control form-control-"
+                id="notas"
+                onChange={cambiar}
+                style={{ borderRadius: "50px", fontSize: "100%" }}
+              >
+                <option value="APROBADO">Aprobado</option>
+                <option value="NO APROBADO">No Aprobado</option>
+                <option value="EN PROCESO">En Proceso</option>
+                <option value="DESERCION">Desercion</option>
+              </select>
+      </div>
+      <div className="form-group mt-4">
+        <div className="text-center"><button type="submit" className="btn btn-success">Agregar</button></div>
+      </div>
+      </div>
+      
+      
+    </form>
+  </div>
+            </div>
+
+</div>
         </section>
       </main>
     </>
