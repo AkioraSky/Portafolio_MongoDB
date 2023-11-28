abc24dc63b2d5eba93c21b483ebff96b378a3e41
miranda <adsoadso17@gmail.com>
2023-11-27 22:59:42 -0500

    Navbar listo

--- front/src/components/main/Navbar.jsx
+++ front/src/components/main/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
 
       <div class="profile">
         <img src="../../src/assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle"/>
-        <h1 class="text-light"><a href="index.html">Alex Smith</a></h1>
+        <h1 class="text-light"><a href="index.html">Aca va el nombre</a></h1>
         <div class="social-links mt-3 text-center">
           <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
           <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
@@ -34,12 +34,12 @@ const Navbar = () => {
 
       <nav id="navbar" className="nav-menu navbar">
         <ul>
-          <li><a href="#hero" className="nav-link scrollto active"><i className="bx bx-home"></i> <span>Home</span></a></li>
-          <li><a href="#about" className="nav-link scrollto"><i className="bx bx-user"></i> <span>About</span></a></li>
-          <li><a href="#resume" className="nav-link scrollto"><i className="bx bx-file-blank"></i> <span>Resume</span></a></li>
-          <li><a href="#portfolio" className="nav-link scrollto"><i className="bx bx-book-content"></i> <span>Portfolio</span></a></li>
-          <li><a href="#services" className="nav-link scrollto"><i className="bx bx-server"></i> <span>Services</span></a></li>
-          <li><a href="#contact" className="nav-link scrollto"><i className="bx bx-envelope"></i> <span>Contact</span></a></li>
+          <li><a href="#hero" className="nav-link scrollto active"><i className="bx bx-home"></i> <span>Proyectos</span></a></li>
+          <li><a href="#about" className="nav-link scrollto"><i className="bx bx-user"></i> <span>Crear Proyectos</span></a></li>
+          <li><a href="#resume" className="nav-link scrollto"><i className="bx bx-file-blank"></i> <span>Estudios</span></a></li>
+          <li><a href="#portfolio" className="nav-link scrollto"><i className="bx bx-book-content"></i> <span>Crear Estudios</span></a></li>
+          <li><a href="#services" className="nav-link scrollto"><i className="bx bx-server"></i> <span>Salir</span></a></li>
+         
         </ul>
       </nav>
     </div>
